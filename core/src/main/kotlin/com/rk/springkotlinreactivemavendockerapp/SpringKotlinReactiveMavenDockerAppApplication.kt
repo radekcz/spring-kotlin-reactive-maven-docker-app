f91935cc3b0a92ec0c053ff382d360057c5f017e
Radek Krizka <radek.krizka@pricefx.com>
2025-03-23 18:46:34 +0100

    feature - fix SpringKotlinReactiveMavenDockerAppApplication

--- core/src/main/kotlin/com/rk/springkotlinreactivemavendockerapp/SpringKotlinReactiveMavenDockerAppApplication.kt
+++ core/src/main/kotlin/com/rk/springkotlinreactivemavendockerapp/SpringKotlinReactiveMavenDockerAppApplication.kt
@@ -4,9 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringKotlinReactiveMavenDockerAppApplication {
+class SpringKotlinReactiveMavenDockerAppApplication
 
-	fun main(args: Array<String>) {
-		runApplication<SpringKotlinReactiveMavenDockerAppApplication>(*args)
-	}
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinReactiveMavenDockerAppApplication>(*args)
 }
