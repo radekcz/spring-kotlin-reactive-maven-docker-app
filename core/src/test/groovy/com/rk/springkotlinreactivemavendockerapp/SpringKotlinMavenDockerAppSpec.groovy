a2325bab9d719546abcb1c25f7da7d321d3ff7af
Radek Krizka <radek.krizka@pricefx.com>
2025-03-23 13:59:43 +0100

    feature - fix SpringKotlinMavenDockerAppSpec test

--- core/src/test/groovy/com/rk/springkotlinreactivemavendockerapp/SpringKotlinMavenDockerAppSpec.groovy
+++ core/src/test/groovy/com/rk/springkotlinreactivemavendockerapp/SpringKotlinMavenDockerAppSpec.groovy
@@ -3,22 +3,27 @@ package com.rk.springkotlinreactivemavendockerapp
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.reactive.server.WebTestClient
 import spock.lang.Specification
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class SpringKotlinMavenDockerAppSpec extends Specification {
 
     @Autowired
-    private MockMvc mockMvc
+    private WebTestClient webTestClient
 
     def "application should start and should response to health-check"() {
-        expect:
-        mockMvc.perform(get("/actuator/health"))
-                .andExpect(status().isOk())
+        when:
+        def result = webTestClient
+                .get()
+                .uri { uriBuilder -> uriBuilder
+                        .path("/actuator/health")
+                        .build()
+                }
+                .exchange()
+
+        then:
+        result.expectStatus().isOk()
     }
 }
